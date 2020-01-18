ed6e6a129f218768494433e37856ae12328d9cf0
Lourguioui <iz_lourguioui@esi.dz>
2020-01-18 23:10:17 +0100

    form

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
 
-export default function App() {
+export default function App () {
+  // const [outputText, setOutputText] = useState('The first React Native app')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.outputContainer}
+        />
+        <Button title='Addd' style={{}} />
+      </View>
+      <View></View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 60
   },
-});
+  outputContainer : {
+    borderColor: 'grey',
+    borderWidth: 0.5,
+    paddingBottom: 15,
+    width: '80%',
+    marginRight: 20,
+    borderRadius: 10,
+    textAlign: 'center'
+  },
+  buttonStyle:{
+
+  }
+})
