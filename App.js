3507aeef8b2027791c500a02af967fcc2eb1f035
Lourguioui <iz_lourguioui@esi.dz>
2020-01-19 07:42:53 +0100

    react hooks

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput, ScrollView } from 'react-native'
 
 export default function App () {
   // const [outputText, setOutputText] = useState('The first React Native app')
+  const [entredGoal, setentredGoal] = useState('')
+  const [goals, setGoals] = useState([])
+  const handleInput = entredGoal => {
+    setentredGoal(entredGoal)
+  }
+  const addGoalHandler = () => {
+    setGoals(currentGoals => [...currentGoals,entredGoal])
+  }
   return (
     <View style={styles.screen}>
       <View
@@ -15,10 +23,14 @@ export default function App () {
         <TextInput
           placeholder='Course Goal'
           style={styles.outputContainer}
+          onChangeText={handleInput}
+          value={entredGoal}
         />
-        <Button title='Addd' style={{}} />
+        <Button title='Addd' style={{}} onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <ScrollView>
+      {goals.map((goal) => <View style={styles.goalsStyle} key={goal}><Text>{goal}</Text></View>)}
+      </ScrollView>
     </View>
   )
 }
@@ -27,7 +39,7 @@ const styles = StyleSheet.create({
   screen: {
     padding: 60
   },
-  outputContainer : {
+  outputContainer: {
     borderColor: 'grey',
     borderWidth: 0.5,
     paddingBottom: 15,
@@ -36,7 +48,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     textAlign: 'center'
   },
-  buttonStyle:{
+  buttonStyle: {},
+  goalsStyle : {
+    padding : 10,
+    marginVertical : 10,
+    backgroundColor : '#bbb',
+    textAlign : "center",
+    fontFamily : "Roboto",
+    shadowColor : "black",
+  
 
   }
 })
